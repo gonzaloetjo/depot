032cf55fdae4c4f6c7dabefc4425210e108dfc35
Gonzalo Etse <gonzaloetjo@gmail.com>
2020-04-16 18:30:20 +0200

    signals et verify

--- Fundamentaux/cryptoasymetric.js
+++ Fundamentaux/cryptoasymetric.js
@@ -1,4 +1,41 @@
+// Utilisation du module crypto pour la cryptographie assymétrique
 const crypto = require('crypto')
+const prompt = require('prompt-sync')({sigint: true});
+var signature = ""
 
-const {privateKey, publicKey} = crypto.generateKeyPairSync('ec', {namedCurve:'secp256k1'})
-console.log(privateKey,publicKey)
+//  const clesRSA = crypto.generateKeyPairSync('rsa', { modulusLength: 4096});
+let key = {}
+function keyGeneration() {
+    const keyECC = crypto.generateKeyPairSync('ec', {namedCurve:'secp256k1'})
+    key = keyECC
+    return key
+}
+
+function signals(message,privateKey){
+    const sign = crypto.createSign('SHA256');
+    sign.write(message);
+    sign.end();
+    signature = sign.sign(privateKey, 'hex');
+    return signature
+}
+
+function verifying(message,publicKey, signature){
+    const verify = crypto.createVerify('SHA256');
+    verify.write(message);
+    verify.end();
+    //console.log(verify.verify(publicKey, signature));
+    return verify.verify(publicKey, signature, 'hex')
+}
+
+console.log(keyGeneration())
+console.log(`your private key is ${key.publicKey}, the public key is ${key.publicKey}`)
+
+let message = Buffer.from(prompt("writte a message "));
+console.log(`your message signature is: [${signals(message,key.privateKey).substr(0,8)}..]`)
+
+let verify_message = Buffer.from(prompt("writte a message to verify key: " ));
+console.log(`The verification is:..`)
+
+console.log(verifying(verify_message,key.publicKey, signature))
+
+
