d3f58d9ff5f92d09095d6cda091cc8975043c8ef
Gonzalo Etse <gonzaloetjo@gmail.com>
2020-04-16 09:35:53 +0200

    changes

--- Fundamentaux/hachage.js
+++ Fundamentaux/hachage.js
@@ -1,13 +1,50 @@
-function pseudohash(str) {
-    let hash = 0 
-    for (let i = 0; i < str.length-1; i++) {
-        hash = (hash + 1) *(str.charCodeAt(i))
-    }
-    console.log(hash)
-    return hash % (256*256)
+const crypto = require('crypto')
+const prompt = require('prompt-sync')({sigint: true});
+
+
+let message = prompt("Send a message: ");
+
+
+function codeVerification(message){
+    let data = Buffer.from(message)
+    let hash = crypto.createHash('sha256').update(data)
+    let code = hash.digest('hex').substr(0,8)
+    return code
 }
 
-console.log(pseudohash("Bonjour"))
-console.log(pseudohash("Bonjour."))
-console.log(pseudohash("Bonjoru"))
-console.log(pseudohash("Bonjour"))
+function verifierCode(message,code){
+    let data = Buffer.from(message)
+    let hash = crypto.createHash('sha256').update(data)
+    if (code === hash.digest('hex').substr(0,8)){
+        return true
+    }
+    else {
+        return false
+    }
+    
+    
+}
+
+function vanite(debut, message){
+    let nonce = 0
+
+    return nonce
+}
+function verifierVanite(debut, message, nonce){
+    
+    
+    return true
+}
+
+
+//let message = "Bonjour à tous"
+let data = Buffer.from(message)
+let hash = crypto.createHash('sha256').update(data)
+console.log(`The message is \'${message}\'. The has is ${hash.digest('hex')}`)
+
+let message_2 = prompt("Verify this message: ");
+let code = codeVerification(message_2)
+console.log(`Verification code is :\' ${code}\', validation : ${verifierCode(message,code)}`)
+
+let debutHash = 'ab'
+console.log(`A hash starting by \'${debutHash}\' peut être obtenu en ajoutant ${vanite(debutHash,message)} au message`)
